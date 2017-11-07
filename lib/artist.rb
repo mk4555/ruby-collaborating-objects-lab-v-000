26ae276ffd110eaa650613132cfb38922fc16d68
Min Kim <mk4555@nyu.edu>
2017-11-07 21:29:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,7 +24,12 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-
+    @@all.each do |artist|
+      if artist.name == name 
+        return artist
+      end
+    end
+    a = Artist.new(name)
   end
 
 end
